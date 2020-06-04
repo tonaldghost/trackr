f55528ba6ca26e1d50e514f3520261c9d4d58f31
Tony Duchars <tonyduchars@tonys-mbp.lan>
2020-06-04 17:48:20 +0100

    td : added them context but bug currently not allowing react import

--- App.js
+++ App.js
@@ -1,36 +1,46 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { themes, ThemeContext } from "./theme/ThemeContext";
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import HomePage from "./components/HomePage";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    theme: themes.light
+  };
+
+  toggleTheme = () => {
+    this.setState(state => ({
+      theme: state.theme === themes.dark ? themes.light : themes.dark
+    }));
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ThemeContext.Provider value={this.state.theme}>
+          <HomePage />
+        </ThemeContext.Provider>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
---
+++ components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+export default function HomePage() {
+  return (
+    <>
+      <Text>Dashboard</Text>
+      <Text>Analysis</Text>
+      <Text>Expenses</Text>
+      <Text>Forecast</Text>
+    </>
+  );
+}
